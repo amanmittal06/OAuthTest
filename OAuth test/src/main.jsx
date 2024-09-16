14ae803a7cec60185f1910a15880c4b7d12e7489
amanmittal06 <amanagrawal061202@gmail.comm>
2024-09-16 08:12:02 +0530

    updated

--- OAuth test/src/main.jsx
+++ OAuth test/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    <GoogleOAuthProvider clientId="269312667183-gpijj1vp2m0l7im2ma7o3dp7ok2enhi0.apps.googleusercontent.com">
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+    </GoogleOAuthProvider>
+  
 )
